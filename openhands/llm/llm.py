57f373499aa6261bdc4f0d0734d4814d16e53523
Eric Ge <ericlege@google.com>
2025-04-15 23:58:48 +0000

    implement retry in llm.py

--- openhands/llm/llm.py
+++ openhands/llm/llm.py
@@ -23,6 +23,7 @@ from litellm.exceptions import (
 from litellm.types.utils import Choices, CostPerToken, ModelResponse, Usage
 from litellm.utils import create_pretrained_tokenizer
 from litellm.types.utils import Message
+from litellm.llms.base_llm.chat.transformation import BaseLLMException
 
 from openhands.core.exceptions import LLMNoResponseError
 from openhands.core.logger import openhands_logger as logger
@@ -39,12 +40,7 @@ from openhands.llm.retry_mixin import RetryMixin
 __all__ = ['LLM']
 
 # tuple of exceptions to retry on
-LLM_RETRY_EXCEPTIONS: tuple[type[Exception], ...] = (
-    RateLimitError,
-    litellm.Timeout,
-    litellm.InternalServerError,
-    LLMNoResponseError,
-)
+LLM_RETRY_EXCEPTIONS: tuple[type[Exception], ...] = (RateLimitError,httpx.HTTPStatusError,BaseLLMException,litellm.InternalServerError)
 
 # cache prompt supporting models
 # remove this when we gemini and deepseek are supported
@@ -326,7 +322,7 @@ class LLM(RetryMixin, DebugMixin):
                     if delta.provider_specific_fields is not None:
                         provider_specific_fields = delta.provider_specific_fields
 
-            logger.info(chunks)
+            logger.info(f'chunks count: {len(chunks)}')
             logger.info(f'joined_content: {joined_content}')
             logger.info(f'role: {role}')
             logger.info(f'tool_call: {tool_calls}')
